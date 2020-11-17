663088cd5caa6ec9b7811000117c4b36c07041b4
06wj <06wj@163.com>
2020-11-17 19:14:08 +0800

    fix shadow demo

--- demos/sdf/shadow/index.glsl
+++ demos/sdf/shadow/index.glsl
@@ -1,35 +1,119 @@
-float random(float n){
-    return fract(sin(n)*1e4);
+const int RAYCAST_STEPS = 200;
+const float RAYCAST_MIN = 1.0;
+const float RAYCAST_MAX = 20.0;
+
+float sdSphere(vec3 pos, float r) {
+    return length(pos) - r;
 }
 
-float noise(float x) {
-    float a = random(floor(x));
-    float b = random(floor(x) + 1.0);
-    float f = fract(x);
-    float u = f * f * (3.0 - 2.0 * f);
-    return mix(a, b, floor(sin(iTime * 5.0)) < -.5 ? f : u);
+float sdBox( vec3 p, vec3 b )
+{
+  vec3 q = abs(p) - b;
+  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
 }
 
-float plot(vec2 st, float pct){
-  return  smoothstep( pct-0.02, pct, st.y) -
-          smoothstep( pct, pct+0.02, st.y);
+float sdPlane(vec3 pos, float y) {
+    return pos.y - y;
 }
 
-void mainImage( out vec4 fragColor, in vec2 fragCoord ){    
-    vec2 pos = (2.0*fragCoord-iResolution.xy)/iResolution.y;
+float map(vec3 pos) {
+    float sdf = sdSphere(pos - vec3(.0, .8, .0), .8);
+    sdf = min(sdf, sdPlane(pos, .0));
+    return sdf;
+}
 
-    float y = noise(pos.x * 2.5 + iTime * 5.) * .5;
-    float y2 = noise(pos.x * 2. + iTime * 3.) * .5;
-    float y3 = noise(pos.x * 1.5 + iTime * 1.) * .8;
+float softShadow( in vec3 rayPos, in vec3 rayDir, in float mint, in float maxt, float k )
+{
+    float res = 1.0;
+    float t = mint;
+    for(int i = 0;i < 16;i ++) {
+        if (t >= maxt) {
+            break;
+        }
 
-    float pct = plot(pos, y);
-    float pct2 = plot(pos, y2);
-    float pct3 = plot(pos, y3);
+        float h = map(rayPos + rayDir*t);
+        if( h<0.001 )
+            return 0.0;
+        res = min( res, k*h/t );
+        t += h;
+    }
+    return fract(iTime * .3) < .5 ? res : 1.0;
+}
 
-    vec3 color = vec3(1.0);
-    color = (1.0 - pct) * color + pct * vec3(0.6, 0.3, 0.9);
-    color = (1.0 - pct2) * color + pct2 * vec3(0.6, 0.9, 0.3);
-    color = (1.0 - pct3) * color + pct3 * vec3(0.9, 0.6, 0.3);
+float raycast(in vec3 rayPos, in vec3 rayDir) {
+    float t = RAYCAST_MIN;
+    for(int i = 0;i < RAYCAST_STEPS;i ++) {
+        if (t > RAYCAST_MAX) {
+            break;
+        }
+        float sdf = map(rayPos + rayDir * t);
+        if (abs(sdf) < 0.0001 * t) {
+            return t;
+        }
 
+        t += sdf;
+    }
+
+    return -1.0;
+}
+
+float getAO(in vec3 pos, in vec3 nor)
+{
+    float occ = 0.0;
+    float sca = 1.0;
+    for( int i=0; i<5; i++ )
+    {
+        float h = 0.01 + 0.12*float(i)/4.0;
+        float d = map( pos + h*nor );
+        occ += (h-d)*sca;
+        sca *= 0.95;
+        if( occ>0.35 ) break;
+    }
+    return clamp( 1.0 - 3.0*occ, 0.0, 1.0 ) * (0.5+0.5*nor.y);
+}
+
+vec3 getNormal(vec3 pos) {
+    vec2 e = vec2(1.0,-1.0)*0.5773*0.0005;
+    return normalize( e.xyy*map(pos + e.xyy)+ 
+                      e.yyx*map(pos + e.yyx)+ 
+                      e.yxy*map(pos + e.yxy)+ 
+                      e.xxx*map(pos + e.xxx)); 
+}
+
+vec3 render(vec3 cameraPos, vec3 dir) {
+    float t = raycast(cameraPos, dir);
+
+    if (t >= .0) {
+        vec3 pos = cameraPos + t * dir;
+        vec3 normal = getNormal(pos);
+        
+        float ao = getAO(pos, normal);
+        float shadow = softShadow(pos, normalize(vec3(.2, .25, -.1)), 0.02, 2.5, 8.0);
+        return vec3(shadow);
+    }
+
+    return vec3(.4);
+}
+
+mat3 setCamera( in vec3 pos, in vec3 target, float cr )
+{
+    vec3 cw = normalize(target-pos);
+    vec3 cp = vec3(sin(cr), cos(cr),0.0);
+    vec3 cu = normalize( cross(cw,cp) );
+    vec3 cv =          ( cross(cu,cw) );
+    return mat3( cu, cv, cw );
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec3 cameraTarget = vec3( .0, .0, .0 );
+    vec3 cameraPos = cameraTarget + vec3(-2.5, 1.3, 4.5);
+    mat3 cameraMat = setCamera( cameraPos, cameraTarget, 0.0 );
+
+    vec2 p = (2.0*fragCoord-iResolution.xy)/iResolution.y;
+    vec3 rayDir = cameraMat * normalize(vec3(p, 2.5));
+
+    vec3 color = render(cameraPos, rayDir);
+    
     fragColor = vec4(color, 1.0);
 }
